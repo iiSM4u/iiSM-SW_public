e499900daf261d5d507c1f121f5dba3dea05f46b
suyeongpark <92278941+suyeong-park@users.noreply.github.com>
2024-07-16 16:47:21 +0900

    일단 이미지와 동영상 띄우는것까지 처리

--- SIMP/mainwindow.cpp
+++ SIMP/mainwindow.cpp
@@ -1,14 +1,128 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
+    , model(std::make_unique<QFileSystemModel>(this))
+    , mediaPlayer(new QMediaPlayer(this))
 {
-    ui->setupUi(this);
+    ui->setupUi(this);    
+
+    mediaPlayer->setVideoOutput(ui->videoFile);
+
+    // Set up the file system model to display only images
+    model->setRootPath("");
+    model->setNameFilters(QStringList() << "*.png" << "*.jpg" << "*.jpeg" << "*.bmp" << "*.gif");
+    model->setNameFilterDisables(false);
+
+    // Set up the QListView
+    ui->lvFrames->setModel(model.get());
+    connect(ui->lvFrames, &QListView::clicked, this, &MainWindow::onSelecteImage);
+    connect(ui->btnLoadFrame, &QPushButton::clicked, this, &MainWindow::onClickLoadFrames);
+
+    connect(ui->btnLoadVideo, &QPushButton::clicked, this, &MainWindow::onClickOpenVideo);
+    connect(ui->btnPlayVideo, &QPushButton::clicked, this, &MainWindow::onClickPlayVideo);
+    connect(ui->btnStopVideo, &QPushButton::clicked, this, &MainWindow::onClickStopVideo);
+
+    connect(mediaPlayer, &QMediaPlayer::mediaStatusChanged, this, &MainWindow::onVideoStatusChanged);
+
+    ui->btnPlayVideo->setEnabled(false);
+    ui->btnStopVideo->setEnabled(false);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::onClickLoadFrames()
+{
+    QString dir = QFileDialog::getExistingDirectory(this, tr("Open Directory"), "", QFileDialog::ShowDirsOnly | QFileDialog::DontResolveSymlinks);
+
+    if (!dir.isEmpty())
+    {
+        ui->lvFrames->setRootIndex(model->setRootPath(dir));
+        ui->lbDirFrames->setText(dir);
+    }
+}
+
+
+void MainWindow::onSelecteImage(const QModelIndex &index)
+{
+    QString filePath = model->filePath(index);
+    QPixmap pixmap(filePath);
+    ui->lbFrameCapture->setPixmap(pixmap.scaled(ui->lbFrameCapture->size(), Qt::KeepAspectRatio, Qt::SmoothTransformation));
+}
+
+
+void MainWindow::onClickOpenVideo()
+{
+    QString fileName = QFileDialog::getOpenFileName(this, tr("Open Video"), "", tr("Video Files (*.mp4 *.avi *.mkv)"));
+
+    if (!fileName.isEmpty())
+    {
+        mediaPlayer->setSource(QUrl::fromLocalFile(fileName));
+        ui->lbPathVideo->setText(fileName);
+
+        ui->btnPlayVideo->setEnabled(true);
+        ui->btnStopVideo->setEnabled(true);
+
+        // 일단 자동으로 시작하게 한다.
+        mediaPlayer->play();
+        MainWindow::SetPlayVideo(true);
+    }
+}
+
+
+void MainWindow::onClickPlayVideo()
+{
+    if (isVideoPlay)
+    {
+        mediaPlayer->pause();
+        MainWindow::SetPlayVideo(false);
+    }
+    else
+    {
+        mediaPlayer->play();
+        MainWindow::SetPlayVideo(true);
+    }
+}
+
+
+void MainWindow::onClickStopVideo()
+{
+    mediaPlayer->stop();
+    MainWindow::SetPlayVideo(false);
+}
+
+void MainWindow::onVideoStatusChanged(QMediaPlayer::MediaStatus status)
+{
+    if (status == QMediaPlayer::EndOfMedia)
+    {
+        MainWindow::SetPlayVideo(false);
+    }
+}
+
+void MainWindow::SetPlayVideo(bool value)
+{
+    if (!value)
+    {
+        ui->btnPlayVideo->setText("Play");
+        isVideoPlay = false;
+    }
+    else
+    {
+        ui->btnPlayVideo->setText("Pause");
+        isVideoPlay = true;
+    }
+}
+
+// void MainWindow::resizeEvent(QResizeEvent* event)
+// {
+//     QMainWindow::resizeEvent(event);
+//     if (!currentPixmap.isNull()) {
+//         ui->lbFrameCapture->setPixmap(currentPixmap.scaled(ui->lbFrameCapture->size(), Qt::KeepAspectRatio, Qt::SmoothTransformation));
+//     }
+// }
--- SIMP/mainwindow.h
+++ SIMP/mainwindow.h
@@ -2,6 +2,14 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QMediaPlayer>
+#include <QFileDialog>
+#include <QFileSystemModel>
+#include <QSortFilterProxyModel>
+#include <memory>
+#include <QLabel>
+#include <QListView>
+#include <QRegularExpression>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -17,7 +25,25 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void onClickLoadFrames();
+    void onSelecteImage(const QModelIndex &index);
+
+    void onClickOpenVideo();
+    void onClickPlayVideo();
+    void onClickStopVideo();
+    void onVideoStatusChanged(QMediaPlayer::MediaStatus status);
+    void SetPlayVideo(bool value);
+
 private:
     Ui::MainWindow *ui;
+    QMediaPlayer *mediaPlayer;
+    std::unique_ptr<QFileSystemModel> model;
+    //std::unique_ptr<QSortFilterProxyModel> proxyModel;
+    //QPixmap currentPixmap;
+
+    // void resizeEvent(QResizeEvent* event) override;
+
+    bool isVideoPlay = false;
 };
 #endif // MAINWINDOW_H
