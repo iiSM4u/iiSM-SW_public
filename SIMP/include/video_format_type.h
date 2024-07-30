7f1e8ea63a6b2c28c8332b9576d0e7a2c00fa323
suyeongpark <92278941+suyeong-park@users.noreply.github.com>
2024-07-30 11:12:26 +0900

    타입 수정

--- SIMP/include/video_format_type.h
+++ SIMP/include/video_format_type.h
@@ -3,19 +3,22 @@
 #include <QString>
 
 enum class VideoFormatType {
-    MP4,
-    WMV,
-    AVI,
+    MJPEG,
+    XVID,
+    MP4V,
+    NONE
 };
+
 
 inline QString toString(VideoFormatType type)
 {
     switch (type)
     {
-    case VideoFormatType::MP4: return "mp4";
-    case VideoFormatType::WMV: return "wmv";
-    case VideoFormatType::AVI: return "avi";
-    default: return "Unknown";
+    case VideoFormatType::MJPEG: return "MJPEG";
+    case VideoFormatType::XVID: return "XVID";
+    case VideoFormatType::MP4V: return "MP4V";
+    case VideoFormatType::NONE: return "NONE";
+    default: return "None";
     }
 }
 
